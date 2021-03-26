8d5c8a9604a1a822f0fa6f1b20a8c43013d3ae25
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-03-25 22:45:29 -0400

    Updated Color methods

--- app/src/main/java/bitmap/transformer/BitMap.java
+++ app/src/main/java/bitmap/transformer/BitMap.java
@@ -10,6 +10,8 @@ public class BitMap {
     public String inputFilePath;
     public String outputFilePath;
     public String transformName;
+    public int height;
+    public int width;
 
     public BitMap (){
         inputFilePath = null;
@@ -21,24 +23,23 @@ public class BitMap {
         this.outputFilePath = outputFilePath;
         this.transformName = transformName;
     }
-//"app/src/main/resources/bitpam.bmp"
+
     public void read(){
         BufferedImage img;
         try
         {
             img = ImageIO.read(new File(this.inputFilePath));
-            int height = img.getHeight();
-            int width = img.getWidth();
+            this.height = img.getHeight();
+            this.width = img.getWidth();
             ArrayList<Color>bits = new ArrayList<>();
-            for(int h =1; h < height; h++){
-                for(int w = 1; w < width; w++){
+            for(int h =1; h < this.height; h++){
+                for(int w = 1; w < this.width; w++){
                     int rgb = img.getRGB(w,h);
                     Color binaryColor = new Color(rgb);
                     bits.add(binaryColor);
                 }
             }
             getRGBA(bits);
-            System.out.println("test");
         }
         catch (IOException e)
         {
@@ -48,11 +49,11 @@ public class BitMap {
         }
     }
     public void createColorList(BufferedImage img){
-        int height = img.getHeight();
-        int width = img.getWidth();
+//        int height = img.getHeight();
+//        int width = img.getWidth();
         ArrayList<Color>bits = new ArrayList<>();
-        for(int h =1; h < height; h++){
-            for(int w = 1; w < width; w++){
+        for(int h =1; h < this.height; h++){
+            for(int w = 1; w < this.width; w++){
                 int rgb = img.getRGB(w,h);
                 Color binaryColor = new Color(rgb);
                 bits.add(binaryColor);
@@ -70,22 +71,58 @@ public class BitMap {
             temp.add(binary.getBlue());
             result.add(temp);
         }
-        System.out.println(result);
+//        System.out.println(result);
         return result;
     }
 
     public void write(){
+        BufferedImage bi ;
+        if(this.outputFilePath != null) {
+            File outputFile = new File(this.outputFilePath);
+            try {
+                bi = ImageIO.read(new File(this.inputFilePath));
+                width = bi.getWidth();
+                height = bi.getHeight();
 
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+                        Color c = new Color(bi.getRGB(j, i));
+                        int red = (int) (c.getRed() * 0.299);
+                        int green = (int) (c.getGreen() * 0.587);
+                        int blue = (int) (c.getBlue() * 0.114);
+                        Color newColor = new Color(0,0,0);
+
+                        if(this.transformName == "gray"){
+                            newColor = grayscale(red,green,blue);
+                        }else if(this.transformName == "invert"){
+                            newColor = invert(red,green,blue);
+                        }else if(this.transformName == "random"){
+                            newColor = randomize(red,green,blue);
+                        }
+                        bi.setRGB(j, i, newColor.getRGB());
+                    }
+                }
+                ImageIO.write(bi,"bmp",outputFile);
+            } catch (IOException e) {
+            }
+        }
+        else
+        {
+            System.out.println("The output filepath has not been set");
+        }
     }
 
-    public void randomize(){
-
+    public Color randomize(int red, int green,int blue){
+        return new Color(red + green + blue,
+                red + green + blue, red + green + blue);
     }
-    public void invert(){
-
+    public Color invert(int red, int green,int blue){
+        return new Color(red + green + blue,
+                red + green + blue, red + green + blue);
     }
-    public void grayscale(){
-
+    public Color grayscale(int red, int green,int blue){
+        return new Color(red + green + blue,
+                red + green + blue, red + green + blue);
     }
 
 }
