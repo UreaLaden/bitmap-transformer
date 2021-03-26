a37dec1d7d0b0538954664e9ae4b5d8df000874b
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-03-26 00:59:49 -0400

    Adjusted tests

--- app/src/main/java/bitmap/transformer/App.java
+++ app/src/main/java/bitmap/transformer/App.java
@@ -3,15 +3,32 @@
  */
 package bitmap.transformer;
 
+import java.util.Map;
+
 public class App {
 
     public static void main(String[] args) {
-        BitMap bitMap = new BitMap();
-        bitMap.inputFilePath = "app/src/main/resources/bitmap.bmp";
-        bitMap.read();
-        bitMap.outputFilePath = "app/src/main/resources/test7.bmp";
-        bitMap.write();
+        BitMap map;
+        switch(args.length)
+        {
+            case 0:
+                map = new BitMap();
+                break;
+            case 1:
+                map = new BitMap(args[0]);
+                map.read();
+                break;
+            case 2:
+                map = new BitMap(args[0],args[1]);
+                map.write();
+                break;
+            case 3:
+                map = new BitMap(args[0],args[1],args[2]);
+                map.write();
+                break;
+            default:
+                System.out.println("Too many arguments");
+        }
     }
-
 
 }
